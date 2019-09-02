e626fda225b32b550de5993f1100bb1a9f7136db
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-02 18:59:22 -0400

    Add end point for getting all hobbits

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+const db = require('./data/db.js');
+const express = require('express');
+
+const server = express();
+
+// Post stuff
+server.post('/api/users', (req, res) => {});
+
+// Get stuff
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'The users information could not be retrieved.' });
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ err: 'The users information could not be retrieved.' });
+        });
+});
+
+server.listen(5000, () => {
+    console.log('Listening on port 5000');
+});
